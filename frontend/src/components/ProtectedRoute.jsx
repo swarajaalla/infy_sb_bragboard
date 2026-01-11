5cabcabbb65d926e9cca505853b503c78a79fc19
SIRIPURAM VINOD KUMAR <siripuramvinodkumar333@gmail.com>
2026-01-12 00:20:20 +0530

    Delete frontend directory

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { isLoggedIn } from "../auth";
-
-export default function ProtectedRoute({ children }) {
-  if (!isLoggedIn()) {
-    return <Navigate to="/login" />;
-  }
-  return children;
-}
